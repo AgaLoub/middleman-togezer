1f0332a6a6e4536285fe2631d173b00422e1fc67
Agathe Loubert <agaloubert@hotmail.com>
2017-12-27 17:02:07 +0100

    changement en cours

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,24 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+// import "../banner";
+
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.ml15 .word',
+//     scale: [14,1],
+//     opacity: [0,1],
+//     easing: "easeOutCirc",
+//     duration: 800,
+//     delay: function(el, i) {
+//       return 800 * i;
+//     }
+//   }).add({
+//     targets: '.ml15',
+//     opacity: 0,
+//     duration: 1000,
+//     easing: "easeOutExpo",
+//     delay: 1000
+//   });
+
